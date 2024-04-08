5a1fe3681042c1fe8065a23d62026e6a0607c509
EStoilov <emiliyan.stoilov@abv.bg>
2024-04-08 07:47:03 +0300

    added Application runner for create and fill the banknotes.txt file

---
+++ cash-dash-module/src/main/java/com/fib/cashdashmodule/CashDashModuleApplication.java
@@ -0,0 +1,35 @@
+package com.fib.cashdashmodule;
+
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+@SpringBootApplication
+public class CashDashModuleApplication implements ApplicationRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CashDashModuleApplication.class, args);
+	}
+
+	@Override
+	public void run(ApplicationArguments args) throws Exception {
+		createBanknotesFile();
+	}
+
+	private void createBanknotesFile() {
+		File file = new File("./banknotes.txt");
+		try (FileWriter writer = new FileWriter(file)) {
+			// Записване на информацията за банкнотите във файла
+			writer.write("Starting Amounts:\n");
+			writer.write("1000 BGN, denomination: 50x10, 10x50\n");
+			writer.write("2000 EUR, denomination: 100x10, 20x50\n");
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+	}
+}
